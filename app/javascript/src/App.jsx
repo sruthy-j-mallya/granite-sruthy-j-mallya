983c71007a3e09f915dba445436cb3188e91facb
sruthy-j-mallya <sruthy.mallya@bigbinary.com>
2022-09-02 13:59:07 +0530

    Added signup feature

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -5,6 +5,7 @@ import { ToastContainer } from "react-toastify";
 
 import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
+import Signup from "components/Authentication/Signup";
 import Dashboard from "components/Dashboard";
 import PageLoader from "components/PageLoader";
 import { CreateTask, ShowTask, EditTask } from "components/Tasks";
@@ -30,6 +31,7 @@ const App = () => {
     <Router>
       <ToastContainer />
       <Switch>
+        <Route exact component={Signup} path="/signup" />
         <Route exact component={EditTask} path="/tasks/:slug/edit" />
         <Route exact component={ShowTask} path="/tasks/:slug/show" />
         <Route exact component={CreateTask} path="/tasks/create" />
