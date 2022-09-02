875745faf1b3774851288395405fa775df6e338f
sruthy-j-mallya <sruthy.mallya@bigbinary.com>
2022-09-02 12:59:14 +0530

    Added Jbuilder template for show action in TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    respond_with_json({ task: @task, assigned_user: @task.assigned_user })
+    render
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks, except: %i[new edit], param: :slug
-  resources :users, only: :index
+  constraints(lambda { |req| req.format == :json }) do
+    resources :tasks, except: %i[new edit], param: :slug
+    resources :users, only: :index
+  end
 
   root "home#index"
   get "*path", to: "home#index", via: :all
